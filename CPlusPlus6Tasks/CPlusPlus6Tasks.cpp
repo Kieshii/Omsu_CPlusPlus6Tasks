e2c3b0cf7d05614425816bbcecea4c2d70ea7f2d
Kieshii <Kieshii@DESKTOP-NS8DE4S>
2022-05-30 03:03:31 +0600

    Добавьте файлы проекта.

---
+++ CPlusPlus6Tasks/CPlusPlus6Tasks.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "TreeDictionary.h"
+
+int main()
+{
+	TreeDictionary* tree = new TreeDictionary();
+	tree->addWord("Privet");
+	tree->addWord("Makarov");
+	tree->addWord("Anya");
+	tree->addWord("Anya");
+	tree->addWord("Papich");
+	tree->addWord("Ivan");
+	tree->addWord("IMIT");
+	tree->addWord("CPlusPlus");
+	tree->addWord("xQc");
+	std::cout << *tree << std::endl;
+	std::cout << "count words in tree:" << tree->countWords() << std::endl;
+	std::cout << "frequency word \"Anya\" :" << tree->findWord("Anya") << std::endl;
+	std::cout << "frequency word \"Kaban\" :" << tree->findWord("Kaban") << std::endl;
+	std::cout << "frequency word \"NET\" :" << tree->findWord("NET") << std::endl;
+	std::cout << "delete words" << std::endl;
+	tree->deleteWord("Papich");
+	tree->deleteWord("Ivan");
+	tree->deleteWord("Anya");
+	std::cout << *tree << std::endl;
+	std::cout << "count word: " << tree->countWords() << std::endl;
+	std::cout << "frequency word \"Anya\" :" << tree->findWord("Anya") << std::endl;
+	std::cout << "frequency word \"Kaban\" :" << tree->findWord("Kaban") << std::endl;
+	std::cout << "frequency word \"Ivan\" :" << tree->findWord("Ivan") << std::endl;
+	system("pause");
+	return 0;
+}
+
+
